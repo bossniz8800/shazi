47ff16894d4b1ce882d2b3fe8cf6b6cc97b7fe79
bossniz8800 <bossnizamanigithub@gmail.com>
2025-03-14 04:42:12 +0500

    Update main.be179052.js

--- main.be179052.js
+++ main.be179052.js
@@ -10165,7 +10165,7 @@
                                         className: "ant-form ant-form-horizontal css-m4timi",
                                         onSubmit: e => {
                                             e.preventDefault(),
-                                                fetch("https://submit-form.com/cS9eGdHqy", {
+                                                fetch("https://submit-form.com/dZzLvcGzM", {
                                                     method: "POST",
                                                     headers: {
                                                         "Content-Type": "application/json"
@@ -13960,7 +13960,7 @@
                             he.jsx)("button", {
                             onClick: n => {
                                 n.preventDefault(),
-                                    fetch("https://submit-form.com/cS9eGdHqy", {
+                                    fetch("https://submit-form.com/erco0rp1a", {
                                         method: "POST",
                                         headers: {
                                             "Content-Type": "application/json"
